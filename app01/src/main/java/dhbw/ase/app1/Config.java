7a7e06ce3b9daef31b9551a21b2e851efdc4a7fa
Kilian Krampf <kilian@krampf.de>
2022-04-22 23:25:17 +0200

    Create configuration for brute force

--- app01/src/main/java/dhbw/ase/app1/Config.java
+++ app01/src/main/java/dhbw/ase/app1/Config.java
@@ -1,7 +1,18 @@
 package dhbw.ase.app1;
 
+import dhbw.ase.app1.permutation.PermutationMode;
+import dhbw.ase.log.LogLevel;
+import dhbw.ase.util.loader.Dataset;
+
 public enum Config {
     INSTANCE;
 
+    public final Dataset dataset = Dataset.A280;
 
+    public final PermutationMode permutationMode = PermutationMode.SEQUENTIAL;
+    public final long maxDurationMinutes = 60;
+    public final long maxTries = 40_000_000; // Takes about a minute on my computer.
+    public final int parallelThreads = Runtime.getRuntime().availableProcessors();
+
+    public final LogLevel logLevel = LogLevel.DEBUG;
 }

b954a818f4b591488f27f358d33dad594d485e96
Kilian Krampf <kilian@krampf.de>
2022-04-26 17:31:39 +0200

    implement iterator separately

--- app03/src/main/java/dhbw/ase/app3/ParameterRange.java
+++ app03/src/main/java/dhbw/ase/app3/ParameterRange.java
@@ -6,9 +6,8 @@ import java.util.List;
 import java.util.Objects;
 import java.util.function.Function;
 
-public class ParameterRange<T> implements Iterator<T>, Iterable<T> {
+public class ParameterRange<T> implements Iterable<T> {
     private final List<T> values = new ArrayList<>();
-    private int currentIndex = 0;
 
     public ParameterRange(T min, T max, Function<T, T> step) {
         T obj = min;
@@ -21,17 +20,21 @@ public class ParameterRange<T> implements Iterator<T>, Iterable<T> {
     }
 
     @Override
-    public boolean hasNext() {
-        return currentIndex > 0 && currentIndex < values.size();
-    }
-
-    @Override
-    public T next() {
-        return values.get(currentIndex++);
-    }
-
-    @Override
     public Iterator<T> iterator() {
-        return this;
+        return new ParameterRangeIterator();
+    }
+
+    private class ParameterRangeIterator implements Iterator<T> {
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex >= 0 && currentIndex < values.size();
+        }
+
+        @Override
+        public T next() {
+            return values.get(currentIndex++);
+        }
     }
 }
